56a4c3aa614731bfea6ebfb0bafac83ec6285313
abccba123 <asadfnu@gmail.com>
2021-05-17 23:02:28 -0400

    Update Chp14_8.py

--- Chp14_8.py
+++ Chp14_8.py
@@ -1,3 +1,9 @@
+'''
+(Display nonduplicate words in ascending order) Write a program that prompts
+the user to enter a text file, reads words from the file, and displays all the nonduplicate
+words in ascending order.
+'''
+
 print("Enter a text file name: ")
 file_input = input()
 try:
