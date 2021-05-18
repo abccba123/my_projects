a018f06e0174653f047629ad3c03f3654b00da1f
abccba123 <asadfnu@gmail.com>
2021-05-17 22:27:30 -0400

    Update Chp3_14.py

--- Chp3_14.py
+++ Chp3_14.py
@@ -1,3 +1,8 @@
+'''
+(Turtle: draw the Olympic symbol ) Write a program that prompts the user to
+enter the radius of the rings and draws an Olympic symbol of five rings of the
+same size with the colors blue, black, red, yellow, and green
+'''
 import turtle
 radius = eval(input("Enter the radius for circle:"))
 turtle.pensize(15)
