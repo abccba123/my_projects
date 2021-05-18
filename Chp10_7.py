26a15cafd8f7e720e92422987eafdb5fed6001ea
abccba123 <asadfnu@gmail.com>
2021-05-17 22:52:03 -0400

    Update Chp10_7.py

--- Chp10_7.py
+++ Chp10_7.py
@@ -1,5 +1,10 @@
-import random
+''''
+(Count single digits) Write a program that generates 1,000 random integers
+between 0 and 9 and displays the count for each number. (Hint: Use a list of ten
+integers, say counts, to store the counts for the number of 0s, 1s, ..., 9s.)
+'''
 
+import random
 Count0 = 0
 Count1 = 0
 Count2 = 0
