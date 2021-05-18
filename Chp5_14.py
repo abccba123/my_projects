36d277f2eca34a6153ef06dde45108aa4174f79a
abccba123 <asadfnu@gmail.com>
2021-05-17 22:31:23 -0400

    Update Chp5_14.py

--- Chp5_14.py
+++ Chp5_14.py
@@ -1,3 +1,8 @@
+'''
+(Find the smallest n such that n2 12,000) Use a while loop to find the smallest
+integer n such that n2 is greater than 12,000.
+'''
+
 # Use a while loop to find the smallest
 # integer n such that n2 is greater than 12,000.
 n = 1
