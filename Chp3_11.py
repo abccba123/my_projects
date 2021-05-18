4af1958cf67cd24acc789f480bbf22139186dae9
abccba123 <asadfnu@gmail.com>
2021-05-17 22:26:55 -0400

    Update Chp3_11.py

--- Chp3_11.py
+++ Chp3_11.py
@@ -1,2 +1,6 @@
+'''
+(Reverse number) Write a program that prompts the user to enter a four-digit integer
+and displays the number in reverse order.
+'''
 number = int(input("Enter a 4 digit integer: "))
 print("The reversed number is:", int(str(number)[::-1]))
