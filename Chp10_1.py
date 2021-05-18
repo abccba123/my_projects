4bd26987c8683ff89d8250af4826a22e6ed80f50
abccba123 <asadfnu@gmail.com>
2021-05-17 22:50:24 -0400

    Update Chp10_1.py

--- Chp10_1.py
+++ Chp10_1.py
@@ -1,3 +1,14 @@
+'''
+(Assign grades) Write a program that reads a list of scores and then assigns grades
+based on the following scheme:
+
+The grade is A if score is  >= best - 10.
+The grade is B if score is  >= best – 20.
+The grade is C if score is  >= best – 30.
+The grade is D if score is  >= best – 40.
+The grade is F otherwise.
+'''
+
 def grades(lst):
     best = int(max(lst))
     m = 0
