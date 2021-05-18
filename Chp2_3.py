3a5b419c548e3af49306eafcabe6ee7af385ae21
abccba123 <asadfnu@gmail.com>
2021-05-17 22:20:37 -0400

    Update Chp2_3.py

--- Chp2_3.py
+++ Chp2_3.py
@@ -1,3 +1,7 @@
+'''
+Write a program that reads a number in feet, converts it
+to meters, and displays the result. One foot is 0.305 meters.
+'''
 x = eval(input("Enter a value for feet to meter conversion:"))
 # formula for conversion of feet to meters
 result = x*.305
