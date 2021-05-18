21aaea73adbec6ff6183c2e6847d96feea258f8c
abccba123 <asadfnu@gmail.com>
2021-05-17 22:44:42 -0400

    Update Chp8_4.py

--- Chp8_4.py
+++ Chp8_4.py
@@ -1,3 +1,12 @@
+'''
+(Occurrences of a specified character) Write a function that finds the number of
+occurrences of a specified character in a string using the following header:
+def count(s, ch):
+The str class has the count method. Implement your method without using the
+count method. For example, count("Welcome", 'e') returns 2. Write a test
+program that prompts the user to enter a string followed by a character and displays
+the number of occurrences of the character in the string.
+'''
 def count(s, ch):
     answer = 0
     for n in range(len(s)):
