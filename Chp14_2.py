742b2ad51c32e1d6bdade3c982a9d150f955a6fb
abccba123 <asadfnu@gmail.com>
2021-05-17 23:00:43 -0400

    Update Chp14_2.py

--- Chp14_2.py
+++ Chp14_2.py
@@ -1,3 +1,12 @@
+'''
+(Count occurrences of numbers) Write a program that reads an unspecified number
+of integers and finds the ones that have the most occurrences. For example, if
+you enter 2 3 40 3 5 4 –3 3 3 2 0, the number 3 occurs most often. Enter all numbers
+in one line. If not one but several numbers have the most occurrences, all of
+them should be reported. For example, since 9 and 3 appear twice in the list 9 30
+3 9 3 2 4, both occurrences should be reported.
+'''
+
 number_input = list(map(int , input("Enter an unspecified numbers of integers: ").split()))
 freq_lst={}
 freq_count = 0
