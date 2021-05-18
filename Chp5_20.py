014342c7679e8cfc0d8c68415a48a2259a6fcd34
abccba123 <asadfnu@gmail.com>
2021-05-17 22:35:51 -0400

    Update Chp5_20.py

--- Chp5_20.py
+++ Chp5_20.py
@@ -1,4 +1,14 @@
-# incomplete program
+'''
+(Display four patterns using loops) Use nested loops that display the following
+patterns in four separate programs:
+Pattern A      Pattern B       Pattern C       Pattern D
+1              1 2 3 4 5 6             1       1 2 3 4 5 6
+1 2            1 2 3 4 5             2 1       1 2 3 4 5
+1 2 3          1 2 3 4             3 2 1       1 2 3 4
+1 2 3 4        1 2 3             4 3 2 1       1 2 3
+1 2 3 4 5      1 2             5 4 3 2 1       1 2
+1 2 3 4 5 6    1             6 5 4 3 2 1       1
+'''
 print("Pattern A")
 for m in range(1,7):
     for n in range(1, m+1):
