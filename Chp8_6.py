543d810a3c65662f2336f844f66f51675b1c5afe
abccba123 <asadfnu@gmail.com>
2021-05-17 22:45:15 -0400

    Update Chp8_6.py

--- Chp8_6.py
+++ Chp8_6.py
@@ -1,3 +1,11 @@
+'''
+(Count the letters in a string) Write a function that counts the number of letters in
+a string using the following header:
+def countLetters(s):
+Write a test program that prompts the user to enter a string and displays the number
+of letters in the string.
+'''
+
 def countLetters(s):
     return len(s)
 
