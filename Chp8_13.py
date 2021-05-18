76270f7d588a25141d4084a9f642e0e4a48423fb
abccba123 <asadfnu@gmail.com>
2021-05-17 22:46:31 -0400

    Update Chp8_13.py

--- Chp8_13.py
+++ Chp8_13.py
@@ -1,3 +1,12 @@
+'''
+(Longest common prefix) Write a method that returns the longest common prefix
+of two strings. For example, the longest common prefix of distance and
+disinfection is dis. The header of the method is:
+def prefix(s1, s2)
+If the two strings have no common prefix, the method returns an empty string.
+Write a main method that prompts the user to enter two strings and displays their
+common prefix.
+'''
 
 def prefix(s1, s2):
     m =0
