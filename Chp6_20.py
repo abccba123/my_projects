fd6d08e9306093a7e549f93386d3d0e0cd42ef22
abccba123 <asadfnu@gmail.com>
2021-05-17 22:43:04 -0400

    Update Chp6_20.py

--- Chp6_20.py
+++ Chp6_20.py
@@ -1,3 +1,8 @@
+'''
+(Geometry: display angles) Rewrite Listing 2.9, ComputeDistance.py, using the
+following function for computing the distance between two points.
+def distance(x1, y1, x2, y2):
+'''
 import math
 def distance(x1, y1, x2, y2):
     distance = ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) ** 0.5
