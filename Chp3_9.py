7e97c6bc98f662df07ad57556afe412978e4cde9
abccba123 <asadfnu@gmail.com>
2021-05-17 22:26:20 -0400

    Update Chp3_9.py

--- Chp3_9.py
+++ Chp3_9.py
@@ -1,4 +1,13 @@
-# extra space in output solve it
+'''
+(Financial application: payroll) Write a program that reads the following information
+and prints a payroll statement:
+Employee’s name (e.g., Smith)
+Number of hours worked in a week (e.g., 10)
+Hourly pay rate (e.g., 9.75)
+Federal tax withholding rate (e.g., 20%)
+State tax withholding rate (e.g., 9%)
+'''
+
 a = input("Enter employee's name:")
 b = eval(input("Enter the number of hours worked in a week:"))
 c = eval(input("Enter hourly pay rate:"))
