0cf4865eddbea54a4fac429882fdfa7510d31e84
abccba123 <asadfnu@gmail.com>
2021-05-17 22:29:03 -0400

    Update Chp4_5.py

--- Chp4_5.py
+++ Chp4_5.py
@@ -1,3 +1,9 @@
+'''
+(Find future dates) Write a program that prompts the user to enter an integer for
+today’s day of the week (Sunday is 0, Monday is 1, ..., and Saturday is 6). Also
+prompt the user to enter the number of days after today for a future day and display
+the future day of the week
+'''
 DaysOfWeek = {0:'Sunday', 1:'Monday', 2:'Tuesday', 3:'Wednesday', 4:'Thursday', 5:'Friday', 6:'Saturday'}
 
 today = int(input("Enter an integer for today's date:"))
