c495608620c2a3b5ea8491789169125e9b4d8761
abccba123 <asadfnu@gmail.com>
2021-05-17 22:21:09 -0400

    Update Chp2_5.py

--- Chp2_5.py
+++ Chp2_5.py
@@ -1,4 +1,9 @@
-
+'''
+Write a program that reads the subtotal and
+the gratuity rate and computes the gratuity and total. For example, if the user
+enters 10 for the subtotal and 15% for the gratuity rate, the program displays 1.5
+as the gratuity and 11.5 as the total.
+'''
 print("Enter the subtotal and the gratuity rate in percentage: ")
 subtotal = eval(input())
 gratuity = eval(input())
