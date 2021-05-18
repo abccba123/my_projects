9e9968c01b8866240e3ff232c630b43e92c840f0
abccba123 <asadfnu@gmail.com>
2021-05-17 22:29:34 -0400

    Update Chp4_6.py

--- Chp4_6.py
+++ Chp4_6.py
@@ -1,3 +1,8 @@
+'''
+(Health application: BMI ) Revise Listing 4.6, ComputeBMI.py, to let users enter
+their weight in pounds and their height in feet and inches. For example, if a person
+is 5 feet and 10 inches, you will enter 5 for feet and 10 for inches.
+'''
 intWeight = eval(input("Enter weight in pounds: "))
 intFeet = eval(input("Enter feet: "))
 intInches = eval(input("Enter inches: "))
