186ff429a224c10ef65fefb04f5e2a93299c2a9f
abccba123 <asadfnu@gmail.com>
2021-05-17 22:45:50 -0400

    Update chp8_11.py

--- chp8_11.py
+++ chp8_11.py
@@ -1,3 +1,11 @@
+'''
+(Reverse a string) Write a function that reverses a string. The header of the function
+is:
+def reverse(s):
+Write a test program that prompts the user to enter a string, invokes the reverse
+function, and displays the reversed string.
+'''
+
 def reverse(s):
     answer = ""
     for m in s:
