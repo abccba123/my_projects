f9a266fabdcba4d3168744048d8ecf95e20cea0c
abccba123 <asadfnu@gmail.com>
2021-05-17 22:28:33 -0400

    Update Chp4_4.py

--- Chp4_4.py
+++ Chp4_4.py
@@ -1,3 +1,8 @@
+'''
+(Game: learn addition) Write a program that generates two integers under 100 and
+prompts the user to enter the sum of these two integers. The program then reports
+true if the answer is correct, false otherwise
+'''
 import random
 
 num1 = random.randint(0, 100)
