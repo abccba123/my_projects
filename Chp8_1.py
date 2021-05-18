fe0572c36befa6eb862d9e9aef7c94545cf9d7bf
abccba123 <asadfnu@gmail.com>
2021-05-17 22:44:12 -0400

    Update Chp8_1.py

--- Chp8_1.py
+++ Chp8_1.py
@@ -1,3 +1,9 @@
+'''
+(Check SSN) Write a program that prompts the user to enter a Social Security
+number in the format ddd-dd-dddd, where d is a digit. The program displays
+Valid SSN for a correct Social Security number or Invalid SSN otherwise.
+✓Point Check
+'''
 def check_SSN(SSN):
     if len(SSN) == 11:
         if SSN[3] and SSN[6] == '-':
