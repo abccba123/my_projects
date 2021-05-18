0218b22b14d023c5ac46fb35a4e8ffd9268ddd6a
abccba123 <asadfnu@gmail.com>
2021-05-17 22:40:56 -0400

    Update Chp6_4.py

--- Chp6_4.py
+++ Chp6_4.py
@@ -1,3 +1,11 @@
+'''
+(Display an integer reversed) Write the following function to display an integer in
+reverse order:
+def reverse(number):
+For example, reverse(3456) displays 6543. Write a test program that prompts
+the user to enter an integer and displays its reversal.
+'''
+
 def reverse(number):
     while number > 0:
         print(number % 10, end='')
